3dbbd4f13e01c6d2a9663040b9ea85b4e7aa48ea
Astrie <uwasterie07@gmail.com>
2023-09-17 13:03:58 +0200

    Initial commit

---
+++ validation.js
@@ -0,0 +1,22 @@
+// The different way of creating the regular expressions in javascript
+
+// first way
+// var reg = /[a-z]/gi;
+
+
+//Second way
+// var reg2 = new RegExp(/[a-z]/,"gi");
+//  The second parameter is for the string of flags like global(g) or insensitive(i)
+
+
+
+
+
+
+// validation script here
+
+const patterns = {
+    telephone : /^\d{7}$/
+};
+
+
